bf2819d9ae4d301015ee1b199ecb4762c0aaa557
Dávid D.Kovács <david.kovacs@tuwien.ac.at>
2025-12-03 14:25:07 +0100

    ISMN runner

--- src/config/run_ismn.py
+++ src/config/run_ismn.py
@@ -5,23 +5,21 @@ matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 import xarray as xr
 import numpy as np
-from pandas import Timestamp
+from pandas import Timestamp,Timedelta
 from utilities.plotting import temp_sm_plot
-sat_sm = xr.open_dataset("/home/ddkovacs/Desktop/personal/daytime_retrievals/datasets/US_2024.nc")
+from config.paths import sat_stack_path,ismn_data_path, path_aux
+from utilities.retrieval_helpers import retrieve_LPRM,tiff_df
 
-
-ISMN_stack = ISMN_Interface('/home/ddkovacs/shares/climers/Projects/CCIplus_Soil_Moisture/07_data/LPRM/debug/daytime_retrieval/ismn_data/SCAN.zip',
-                    parallel=True)
+sat_data = xr.open_dataset(sat_stack_path)
+ISMN_stack = ISMN_Interface(ismn_data_path, parallel=True)
 NETWORK_stack = ISMN_stack["SCAN"]
-
 
 station_user = "Abrams"
 ts_cutoff = Timestamp("2015-01-01")
 depth_selection = Depth(0., 0.1)
 
 
-stat_list = [ 'Lind#1', 'Lindsay', 'LittleRedFox', 'LittleRiver', 'Livingston-UWA', 'LosLunasPMC', 'LovellSummit', 'LovelockNNR', 'LowerMulchatna', 'LyeBrook', 'MahantangoCk', 'MammothCave', 'ManaHouse', 'Mandan#1', 'Manderfield', 'MarbleCreek', 'MaricaoForest', 'MarkTwainHS', 'MascomaRiver', 'Mason#1', 'Mayday', 'McAllisterFarm',]
-
+stat_list = [ 'Lind#1']
 for i in stat_list:
     STATION = NETWORK_stack[i]
     print(i)
@@ -46,7 +44,7 @@ for i in stat_list:
 
 
 
-        data =  sat_sm.sel(
+        data =  sat_data.sel(
             LAT =STATION.lat,
             LON =STATION.lon,
             method = "nearest"
@@ -74,3 +72,38 @@ for i in stat_list:
         )
     except:
         continue
+
+
+##
+SINGLE_STATION = NETWORK_stack[station_user]
+
+_sat_data = sat_data.sel(
+            LAT =SINGLE_STATION.lat,
+            LON =SINGLE_STATION.lon,
+            method = "nearest"
+        ).expand_dims(['LAT','LON']).to_dataframe()
+
+
+_sat_data = _sat_data.drop(columns = ["SM_ADJ"])
+
+doy = Timestamp("2024-06-15")
+sat_day = _sat_data.xs(doy, level="time")
+aux_df = tiff_df(path_aux)
+
+lprm_day = retrieve_LPRM(sat_day,
+                         aux_df,
+                         "AMSR2",
+                         "X"
+                         ).to_dataframe()
+
+lprm_day = lprm_day.drop(columns = ["BLD","SND","CLY","DIF_SMX-ADJ"])
+
+conditions_sm = (ISMN_stack.metadata['variable'].val == 'soil_moisture') & \
+             (ISMN_stack.metadata['instrument'].depth_to < 0.1) & \
+             (ISMN_stack.metadata['instrument'].depth_from >= 0) & \
+             (ISMN_stack.metadata["timerange_to"].val > doy + Timedelta(days=1)) & \
+             (ISMN_stack.metadata['station'].val == station_user)
+
+ids = ISMN_stack.metadata[conditions_sm].index.to_list()
+ts, meta = ISMN_stack.read(ids, return_meta=True)
+
